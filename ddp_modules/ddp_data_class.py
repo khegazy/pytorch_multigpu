39042d3ffc9227ec27a0a4f7b160c91092594dd9
Khegazy <KareemHegz@gmail.com>
2023-10-22 21:45:23 -0700

    Added dataset evaluation function for multigpu.

--- ddp_modules/ddp_data_class.py
+++ ddp_modules/ddp_data_class.py
@@ -49,3 +49,65 @@ def get_dataloader(
             prefetch_factor=prefetch_factor
         )
 
+
+def evaluate_data(
+        model,
+        dataloader,
+        metrics,
+        is_ddp,
+        device,
+        output_rank=None,
+        rank=None,
+        names=None,
+    ):
+    if output_rank is not None and rank is None:
+        raise ValueError(
+            "If output_rank is specified then the rank of the calling"\
+            + " process must be specified."
+        )
+
+    n_total_samples = 0
+    output_sum = 0
+    for batch_idx, batch in enumerate(dataloader):
+        data_x, data_y = batch
+        data_x = data_x.to(device)
+        data_y = data_y.to(device)
+        n_samples = data_x.shape[0]
+        
+        output = metrics(model(data_x), data_y)
+        output_sum += torch.tensor(output)*n_samples
+        n_total_samples += n_samples
+    
+    # Reduce values onto the master or all gpus via summation if using DDP
+    reduced_values = torch.concat(
+        [output_sum, torch.tensor([n_total_samples])]
+    ).to(device)
+    if is_ddp:
+        if output_rank is not None:
+            dist.reduce(reduced_values, output_rank, async_op=False)
+        else:
+            dist.all_reduce(reduced_values, async_op=False)
+    
+    # If names is specified then return a dictionary of the named results
+    if names is None:
+        results = reduced_values[:-1]/reduced_values[-1]
+    else:
+        if len(names) != len(reduced_values) - 1:
+            raise ValueError("When specifying the metric names one"\
+                " must give names for all metrics."
+            )
+        results = {
+            names[idx] : reduced_values[idx]/reduced_values[-1]\
+                for idx in range(len(names))
+        }
+    
+    if output_rank is None or output_rank == rank:
+        return results
+    else:
+        return None
+
+
+
+
+
+
